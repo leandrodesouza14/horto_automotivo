cc2025190e317b1a2b2b7ea4d0413c5fb5df4a5b
leandrodesouza14 <leandrodesouza14@gmail.com>
2023-01-26 17:35:00 -0300

    Implementação da função de selecionar o carro do status

---
+++ php_action/functions/select_car_status.php
@@ -0,0 +1,24 @@
+<?php
+
+if (isset($idcarro) && !empty($idcarro)) {
+    include_once 'php_action/functions/function_clear.php';
+    include_once 'php_action/db_connect.php';
+    $id = clear($idcarro);
+    $sql = "SELECT * FROM carros WHERE id = '$id'";
+    $resultado = mysqli_query($connect, $sql);
+        if (mysqli_num_rows($resultado) === 1) {
+        function selectCar($resultado){
+            $array = mysqli_fetch_array($resultado);
+            return array($array['montadora'] . " " . $array['modelo'] . " " . $array['ano'] . " " . $array['cor'], $array['id']);
+        }
+        } else {
+            mysqli_close($connect);
+            $_SESSION['mensagem'] = "O carro selecionado não existe!";
+            header("Location: ../horto_automotivo/index.php?erro");
+            exit;
+        }
+} else {
+    $_SESSION['mensagem'] = "Você precisa selecionar um carro!";
+    header("Location: ../horto_automotivo/index.php?erro");
+    exit;
+}
--- status.php
+++ status.php
@@ -3,10 +3,14 @@
 	include_once 'includes/header.php';
 	// Inclue o script de mensagens
 	include_once 'includes/message.php';
-	// Inclusão da pesquisa das observações do carro selecionado
+	// Inclusão da pesquisa do status do carro selecionado
 	require_once 'php_action/functions/select_status.php';
-	// Definição do array obs
+	// Definição do array status
 	$status = selectStatus($resultado);
+	// Inclusão da função selectCar
+	$idcarro = $status['id_carro'];
+	require_once 'php_action/functions/select_car_status.php';
+	$carro = selectCar($resultado);
 	// Inclusão da função datatime
 	require_once 'php_action/functions/datatime.php';
 
@@ -34,7 +38,7 @@
 					</thead>
 					<tbody>
 						<tr>
-							<td></td>
+							<td><a href="car.php?id=<?php echo $carro[1]; ?>"><?php echo $carro[0]; ?></a></td>
 							<td><?php echo $status['observacao']; ?></td>
 							<td><?php echo $status['descricao']; ?></td>
 							<td>
